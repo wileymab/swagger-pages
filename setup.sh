da9ea803b1db7a940a5a6aaa1b3e514a8c4432ca
Matt Wiley <matt.wiley@8451.com>
2019-05-28 14:26:39 -0400

    Added setup script.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-
 wget https://github.com/swagger-api/swagger-ui/archive/v3.22.2.zip
 unzip v3.22.2.zip
-cp -r swagger-ui-3.22.2/dist swagger
+cp -r swagger-ui-3.22.2/dist .
 
 sed -e 's^https://petstore.swagger.io/v2/swagger.json^swagger.yaml^' \
     -e 's^\./^dist/^g' \
-    swagger/index.html > index.html
+    dist/index.html > index.html
 
 rm -rf swagger-ui-3.22.2
 rm -rf v3.22.2.zip
