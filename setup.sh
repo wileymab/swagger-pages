4b23baf096132a93585674589fe4b255873c24b6
Matt Wiley <matt.wiley@8451.com>
2019-05-29 09:22:12 -0400

    Added line i setup to pull the starter swagger yaml.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+
 wget https://github.8451.com/raw/m441527/swagger-pages/master/swagger.yaml?token=AAAAOy9XYaKf2sofcuuTGlwRgUbI8m2jks5c977jwA%3D%3D
+mv swagger.yaml?token=AAAAOy9XYaKf2sofcuuTGlwRgUbI8m2jks5c977jwA%3D%3D swagger.yaml
+
 wget https://github.com/swagger-api/swagger-ui/archive/v3.22.2.zip
 unzip v3.22.2.zip
 cp -r swagger-ui-3.22.2/dist .
-
 sed -e 's^https://petstore.swagger.io/v2/swagger.json^swagger.yaml^' \
     -e 's^\./^dist/^g' \
     dist/index.html > index.html
